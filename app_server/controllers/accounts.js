199fc8818e402bdc72551561f0b6a12c2da8d7f6
t00222338 <lydia.sheehan2@students.ittralee.ie>
2024-10-22 12:44:46 +0100

    #Lab 3 finished lab 3 and broke my buttons

--- app_server/controllers/accounts.js
+++ app_server/controllers/accounts.js
@@ -1,10 +1,24 @@
 /* GET login page */
 const login = function(req, res){
-    res.render('login', { title: 'Log In'});
+    res.render('login', { 
+        title: 'Log In',
+        subtitle: 'Log in to jump into a world of squatch',
+        message: 'Not a member?',
+        link: {
+            text: 'Click here to Register',
+            link: '/register/'
+        }});
 };
 /* GET register page */
 const register = function(req, res){
-    res.render('register', { title: 'Register'});
+    res.render('register', { 
+        title: 'Register',
+        subtitle: 'Register to jump into a world of squatch',
+        message: 'Already a member?',
+        link: {
+            text: 'Click here to Log In',
+            link: '/login/'
+        }});
 };
 module.exports = {
     login,
