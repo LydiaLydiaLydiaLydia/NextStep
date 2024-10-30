c764e7c8be763c2719522685c32d13c9a0d44468
Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2024-10-30 13:05:26 +0000

    #Lab Building the API Exercise 2

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
+//var indexRouter = require('./app_server/routes/index');
+const apiRouter = require('/app_api/routes/index');
 
 require('./app_server/models/db');
 
@@ -20,7 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
