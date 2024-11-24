e4150048912d6f4e09df49b2d3af871a6028c60e
Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2024-11-24 17:57:09 +0000

    #Lab07 finished

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 require('./app_api/models/db');
 
-//var indexRouter = require('./app_server/routes/index');
+var indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
 
 
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -1,54 +1,34 @@
-/* GET home page */
-const sightings = function(req, res){
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
+if (process.env.NODE_ENV === 'production'){
+    apiOptions.server = 'https://nextstep-2ngy.onrender.com';
+}
+
+const _renderHomepage = function(req, res, responseBody){
     res.render('index', { 
         title: 'Sightings',
         pageHeader:{
             title: 'The Bigfoot Tracking App',
             strapline: 'Figure out his Next Step'
         },
-        sightings: [{
-            title: 'Old Copper Mine',
-            username: 'FootFinder16',
-            profilePic: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.renewinstitute.pl%2Fwp-content%2Fuploads%2F2022%2F03%2Fbeauty-portrait-ginger-woman-with-long-hair-posing-with-green-leaf-1-1-100x100-1.png&f=1&nofb=1&ipt=309edf12fdf12b0a6310536effa71a22ed5e318e1e72f3ce57fab78989a446e6&ipo=images",
-            text: 'Bigfoot, also commonly referred to as Sasquatch, is a large and hairy human-like mythical creature said to inhabit forests in North America, particularly in the Pacific Northwest. Bigfoot is featured in both American and Canadian folklore, and since the mid-20th century has grown into a cultural icon, permeating popular culture and becoming the subject of its own distinct subculture.',
-            timePosted: '15:37 24 June 2024',
-            coords: [52.016439, -9.575893],
-            verifiedBy: ['BabyFoot23'],
-            mainPic:  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.indy100.com%2Fmedia-library%2Fdramatic-footage-claims-to-show-new-sighting-of-bigfoot-so-do.jpg%3Fid%3D33440809%26width%3D1200%26height%3D600%26coordinates%3D0%252C100%252C0%252C101&f=1&nofb=1&ipt=4ccee9aab8897680e0d7944a489450ea54a0861175297c89c5f94242a3a4be09&ipo=images",
-            bigPics: [
-                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbigfootbase.com%2Fwp-content%2Fuploads%2F2015%2F12%2Fbigfootbase-skunkape-myakka.jpg&f=1&nofb=1&ipt=0bef99aed2f6d9e145f915cc9d8d5557cdb1611f15471a4dec273fab6adb570e&ipo=images",
-                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gannett-cdn.com%2F-mm-%2Fcab617ec2802c2f1834af8114e8031529103a208%2Fc%3D0-101-595-437%2Flocal%2F-%2Fmedia%2F2016%2F09%2F14%2FUSATODAY%2FUSATODAY%2F636094493769555435-BigfootTweet.JPG%3Fwidth%3D3200%26height%3D1680%26fit%3Dcrop&f=1&nofb=1&ipt=8d9fbe4e985bf7a6121c141556a5e7fe47f0ab37f4c288bcc155632dd9b8b4b4&ipo=images"
-            ]
-        },
-        {
-            title: 'Glendalough',
-            username: 'FootFinder16',
-            profilePic: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.renewinstitute.pl%2Fwp-content%2Fuploads%2F2022%2F03%2Fbeauty-portrait-ginger-woman-with-long-hair-posing-with-green-leaf-1-1-100x100-1.png&f=1&nofb=1&ipt=309edf12fdf12b0a6310536effa71a22ed5e318e1e72f3ce57fab78989a446e6&ipo=images",
-            text: 'Bigfoot, also commonly referred to as Sasquatch, is a large and hairy human-like mythical creature said to inhabit forests in North America, particularly in the Pacific Northwest. Bigfoot is featured in both American and Canadian folklore, and since the mid-20th century has grown into a cultural icon, permeating popular culture and becoming the subject of its own distinct subculture.',
-            timePosted: '15:37 24 June 2024',
-            coords: [53.010728, -6.353684],
-            verifiedBy:[],
-            mainPic:  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.indy100.com%2Fmedia-library%2Fdramatic-footage-claims-to-show-new-sighting-of-bigfoot-so-do.jpg%3Fid%3D33440809%26width%3D1200%26height%3D600%26coordinates%3D0%252C100%252C0%252C101&f=1&nofb=1&ipt=4ccee9aab8897680e0d7944a489450ea54a0861175297c89c5f94242a3a4be09&ipo=images",
-            bigPics: [
-                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbigfootbase.com%2Fwp-content%2Fuploads%2F2015%2F12%2Fbigfootbase-skunkape-myakka.jpg&f=1&nofb=1&ipt=0bef99aed2f6d9e145f915cc9d8d5557cdb1611f15471a4dec273fab6adb570e&ipo=images",
-                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gannett-cdn.com%2F-mm-%2Fcab617ec2802c2f1834af8114e8031529103a208%2Fc%3D0-101-595-437%2Flocal%2F-%2Fmedia%2F2016%2F09%2F14%2FUSATODAY%2FUSATODAY%2F636094493769555435-BigfootTweet.JPG%3Fwidth%3D3200%26height%3D1680%26fit%3Dcrop&f=1&nofb=1&ipt=8d9fbe4e985bf7a6121c141556a5e7fe47f0ab37f4c288bcc155632dd9b8b4b4&ipo=images"
-            ]
-        },
-        {
-            title: 'Shop Street',
-            username: 'FootFinder16',
-            profilePic: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.renewinstitute.pl%2Fwp-content%2Fuploads%2F2022%2F03%2Fbeauty-portrait-ginger-woman-with-long-hair-posing-with-green-leaf-1-1-100x100-1.png&f=1&nofb=1&ipt=309edf12fdf12b0a6310536effa71a22ed5e318e1e72f3ce57fab78989a446e6&ipo=images",
-            text: 'Bigfoot, also commonly referred to as Sasquatch, is a large and hairy human-like mythical creature said to inhabit forests in North America, particularly in the Pacific Northwest. Bigfoot is featured in both American and Canadian folklore, and since the mid-20th century has grown into a cultural icon, permeating popular culture and becoming the subject of its own distinct subculture.',
-            timePosted: '15:37 24 June 2024',
-            coords: [53.272612, -9.053047],
-            verifiedBy:[],
-            mainPic:  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.indy100.com%2Fmedia-library%2Fdramatic-footage-claims-to-show-new-sighting-of-bigfoot-so-do.jpg%3Fid%3D33440809%26width%3D1200%26height%3D600%26coordinates%3D0%252C100%252C0%252C101&f=1&nofb=1&ipt=4ccee9aab8897680e0d7944a489450ea54a0861175297c89c5f94242a3a4be09&ipo=images",
-            bigPics: [
-                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbigfootbase.com%2Fwp-content%2Fuploads%2F2015%2F12%2Fbigfootbase-skunkape-myakka.jpg&f=1&nofb=1&ipt=0bef99aed2f6d9e145f915cc9d8d5557cdb1611f15471a4dec273fab6adb570e&ipo=images",
-                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gannett-cdn.com%2F-mm-%2Fcab617ec2802c2f1834af8114e8031529103a208%2Fc%3D0-101-595-437%2Flocal%2F-%2Fmedia%2F2016%2F09%2F14%2FUSATODAY%2FUSATODAY%2F636094493769555435-BigfootTweet.JPG%3Fwidth%3D3200%26height%3D1680%26fit%3Dcrop&f=1&nofb=1&ipt=8d9fbe4e985bf7a6121c141556a5e7fe47f0ab37f4c288bcc155632dd9b8b4b4&ipo=images"
-            ]
-        }]
+        sightings: responseBody
     });
+}
+
+/* GET home page */
+const sightings = function(req, res){
+    const path = '/api/sightings';
+    const requestOptions = {
+        url : apiOptions.server + path,
+        method: 'GET',
+        json: {},
+        qs : {}
+    };
+    request(requestOptions, (err, response, body) => {
+        _renderHomepage(req, res, body);
+    })
 };
 module.exports = {
     sightings
