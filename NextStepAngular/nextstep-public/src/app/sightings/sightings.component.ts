d0748f7814c41887f999cf86435772d512a874f7
Lydia <lydia.sheehan2@students.ittralee.ie>
2024-11-26 10:00:04 +0000

    #Angular lab number 1: finishing exercise 19

--- NextStepAngular/nextstep-public/src/app/sightings/sightings.component.ts
+++ NextStepAngular/nextstep-public/src/app/sightings/sightings.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export class Sighting {
+  title!: string;
+  postedBy!:string;
+  text!: string;
+  timePosted!: Date;
+  coords!: number[];
+  verifiedBy!: string;
+  mainPic!: string;
+  bigPics!: string[];
+}
 
 @Component({
   selector: 'app-sightings',
@@ -7,6 +18,19 @@ import { Component } from '@angular/core';
   templateUrl: './sightings.component.html',
   styleUrl: './sightings.component.css'
 })
-export class SightingsComponent {
+export class SightingsComponent implements OnInit{
+  constructor() {}
+  
+  sighting: Sighting = {
+    title: "Old Copper Mine",
+    postedBy: 'BigSearcher12',
+    text: 'Bigfoot, also commonly referred to as Sasquatch, is a large and hairy human-like mythical creature said to inhabit forests in North America, particularly in the Pacific Northwest. Bigfoot is featured in both American and Canadian folklore, and since the mid-20th century has grown into a cultural icon, permeating popular culture and becoming the subject of its own distinct subculture.',
+    timePosted: new Date("20:52 27 October 2024"),
+    coords: [0,0],
+    verifiedBy: "FootFinder64",
+    mainPic: '',
+    bigPics: []
+  }
 
+  ngOnInit(){}
 }
