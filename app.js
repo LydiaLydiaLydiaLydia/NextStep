d40d85aa9f3dc59588e087cca7ad2ce559f0d197
Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2024-12-09 13:18:21 +0000

    app.js

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ app.use('/api', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+app.use('/api', function(req, res, next){
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+})
   
 
 app.use('/', indexRouter);
